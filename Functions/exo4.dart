36fa2e2bb0e3716b4eaf4bb21a034c110908a9a1
Laugh <70723697+xLaugh@users.noreply.github.com>
2026-03-06 16:55:02 +0100

    //3

--- Functions/exo4.dart
+++ Functions/exo4.dart
@@ -6,8 +6,12 @@ num sum(num a, num b) {
   return a + b;
 }
 
-num sumMany(num a, num b) {
-  return a + b;
+num sumMany(Iterable<num> numbers) {
+  num total = 0;
+  for (final number in numbers) {
+    total += number;
+  }
+  return total;
 }
 
 void main() {
@@ -20,4 +24,14 @@ void main() {
   print('La somme de -400 et 9 est : ${sum(-400, 9)}');
 
   //3
+  print(
+    'La somme de plusieurs nombres (1, 2, 3, 4, 5) est : ${sumMany([1, 2, 3, 4, 5])}',
+  );
+  print(
+    'La somme de plusieurs nombres (10, 20.5, -5) est : ${sumMany([10, 20.5, -5])}',
+  );
+  print(
+    'La somme de plusieurs nombres (un seul nombre 13) est : ${sumMany([13])}',
+  );
+  print('La somme de plusieurs nombres (aucun nombre) est : ${sumMany([])}');
 }
